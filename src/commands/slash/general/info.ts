f632ac1527060fcfef5e265cba16f37c11a5b70e
XtroBag <sgray20066@gmail.com>
2023-07-06 14:56:33 -0400

    totally fixed info.ts

--- src/commands/slash/general/info.ts
+++ src/commands/slash/general/info.ts
@@ -1,15 +1,13 @@
 import {
   ActivityType,
-  // ActivityType,
   ApplicationCommandOptionType,
   ChatInputCommandInteraction,
   EmbedBuilder,
   codeBlock,
-  // codeBlock,
 } from "discord.js";
 import { SlashClass } from "../../../structures/slash.js";
-import { Badges, Colors, Emojis } from "../../../../config.js";
-// import { Guild } from "../../../database/modals/guild.js";
+import { Badges, Emojis, Colors } from "../../../../config.js";
+
 export default new SlashClass({
   data: {
     name: "info",
@@ -42,14 +40,13 @@ export default new SlashClass({
     cooldown: 5,
     guildOnly: false,
   },
-  execute: async (_client, int: ChatInputCommandInteraction<'cached'>) => {
+  execute: async (_client, int: ChatInputCommandInteraction<"cached">) => {
     const choice = int.options.getSubcommand();
     const user = int.options.getUser("user");
     const member = int.options.getMember("user");
 
-
     if (choice === "user") {
-      let flags = user.flags.toArray();
+      let flags = user?.flags.toArray();
       let badges = [];
       await Promise.all(
         flags.map((badge) => {
@@ -96,7 +93,7 @@ export default new SlashClass({
 
       try {
         const userData = await fetch(
-          `https://japi.rest/discord/v1/user/${user.id}`
+          `https://japi.rest/discord/v1/user/${user?.id}`
         );
         const { data } = await userData?.json();
 
@@ -107,34 +104,10 @@ export default new SlashClass({
             })
           );
         }
-
       } catch (err) {
-        console.log('japi api went down when trying to check this user')
+        console.log("japi api went down when trying to check this user");
       }
-      if (user.bot) {
-        const botFetch = await fetch(
-          `https://discord.com/api/v10/applications/${user.id}/rpc`
-        );
-        const json = await botFetch.json();
-        const flagsBot = json.flags;
-        const gateways = {
-          APPLICATION_COMMAND_BADGE: 1 << 23,
-          APPLICATION_AUTO_MODERATION_RULE_CREATE_BADGE: 1 << 6,
-        };
-        const arrayFlags = [];
-        for (let i in gateways) {
-          const bit = gateways[i];
-          if ((flagsBot & bit) === bit) arrayFlags.push(i);
-        }
-        if (arrayFlags.includes("APPLICATION_COMMAND_BADGE")) {
-          badges.push(Badges.SupportsCommands);
-        }
-        if (
-          arrayFlags.includes("APPLICATION_AUTO_MODERATION_RULE_CREATE_BADGE")
-        ) {
-          badges.push(Badges.Automod);
-        }
-      }
+
       if (
         !user.discriminator ||
         user.discriminator === "0" ||
@@ -142,43 +115,95 @@ export default new SlashClass({
       ) {
         badges.push(Badges.Username);
       }
+
       const devices = member?.presence?.clientStatus || {};
       const device = () => {
         const entries = Object.entries(devices)
           .map((value) => {
-            if (value[0] === 'desktop') {
-              return `\n${Emojis.Blank}${value[0].charAt(0).toUpperCase() +
-                value[0].slice(1)}: ${Emojis.Check}`
+            if (value[0] === "desktop") {
+              return `\n${Emojis.Blank}${
+                value[0].charAt(0).toUpperCase() + value[0].slice(1)
+              }: ${Emojis.Check}`;
             }
-            if (value[0] === 'mobile') {
-              return `\n${Emojis.Blank}${value[0].charAt(0).toUpperCase() +
-                value[0].slice(1)}: ${Emojis.Check}`
+            if (value[0] === "mobile") {
+              return `\n${Emojis.Blank}${
+                value[0].charAt(0).toUpperCase() + value[0].slice(1)
+              }: ${Emojis.Check}`;
             }
-            if (value[0] === 'web') {
-              return `\n${Emojis.Blank}${value[0].charAt(0).toUpperCase() +
-                value[0].slice(1)}: ${Emojis.Check}`
+            if (value[0] === "web") {
+              return `\n${Emojis.Blank}${
+                value[0].charAt(0).toUpperCase() + value[0].slice(1)
+              }: ${Emojis.Check}`;
             }
-          }
-          ).join(' ')
-        return `${entries || Emojis.Cross}`
+          })
+          .join(" ");
+        return `${entries || Emojis.Cross}`;
+      };
+
+      //-----------------------------------------------------------------------
+
+      const botFetch = await fetch(
+        `https://discord.com/api/v10/applications/${user.id}/rpc`
+      );
+
+      const json = await botFetch.json();
+      const flagsBot = json.flags;
+
+      const gateways = {
+        APPLICATION_COMMAND_BADGE: 1 << 23,
+        APPLICATION_AUTO_MODERATION_RULE_CREATE_BADGE: 1 << 6,
+      };
+
+      const arrayFlags = [];
+
+      for (let i in gateways) {
+        const bit = gateways[i];
+        if ((flagsBot & bit) === bit) arrayFlags.push(i);
       }
-      if (member) {
-        if (user.bot) {
+
+      if (arrayFlags.includes("APPLICATION_COMMAND_BADGE")) {
+        badges.push(Badges.SupportsCommands);
+      }
+      if (
+        arrayFlags.includes("APPLICATION_AUTO_MODERATION_RULE_CREATE_BADGE")
+      ) {
+        badges.push(Badges.Automod);
+      }
+
+      //-----------------------------------------------------------------------
+
+      try {
+        if (user?.bot) {
           const botEmbed = new EmbedBuilder()
-            .setTitle('Bot Information')
-            .setDescription(`Welcome to ${member.user.username}'s profile`)
-            .addFields([{
-              name: "General",
-              value: "\nBadges:" +
-                ` ${badges.join(" ") || Emojis.Cross}` +
-                "\nVerified:" +
-                ` ${user.flags.has('VerifiedBot') ? `${Badges.VerifiedBot}` : `${Emojis.Cross}`}`
-            }]);
+            .setTitle("Bot Information")
+            .setDescription(`Welcome to ${user?.username}'s profile`)
+            .setThumbnail(user.displayAvatarURL({ extension: 'png'}))
+            .addFields([
+              {
+                name: "General",
+                value:
+                  "\nBadges:" +
+                  ` ${badges.join(" ") || Emojis.Cross}` +
+                  "\nVerified:" +
+                  ` ${
+                    user?.flags.has("VerifiedBot")
+                      ? `${Badges.VerifiedBot}`
+                      : `${Emojis.Cross}`
+                  }`,
+              },
+            ])
+            .setColor(Colors.Normal);
 
-
-          int.reply({ embeds: [botEmbed], ephemeral: true })
+          return await int.reply({ embeds: [botEmbed] });
         }
-        let bot: Boolean | String;
+      } catch (err) {
+        console.log(err, "Had issue grabbing the bot information");
+      }
+
+      //--------------------------------------------------------------------------------
+
+      if (member) {
+        let bot: String;
         if (member.user.bot) {
           bot = Emojis.Check;
         } else {
@@ -198,45 +223,49 @@ export default new SlashClass({
         };
 
         const memberEmbed = new EmbedBuilder()
-          .setTitle('testing')
+          .setTitle("testing")
           .setDescription(`> ${Emojis.Information} **Member Information**`)
           .setThumbnail(member.displayAvatarURL({ extension: "png" }))
           .addFields([
             {
               name: "General:",
               value:
-                `\n${Emojis.Profile} Profile:` +
+                `\nProfile:` +
                 ` \`\`${member.user.username}\`\`
-                  ${Emojis.Blank} ${Emojis.RightArrow} Badges: ${badges.join(" ") || Emojis.Cross}
-                  ${Emojis.Blank} ${Emojis.RightArrow} ID: ${member.id}
-                  ${Emojis.Blank} ${Emojis.RightArrow} Status: ${mode[member.presence?.status ?? "offline"]} ${status[member.presence?.status ?? "offline"]
-                }` +
+                  ${Emojis.Blank} Badges: ${badges.join(" ") || Emojis.Cross}
+                  ${Emojis.Blank} ID: ${member.id}
+                  ${Emojis.Blank} Status: ${
+                  mode[member.presence?.status ?? "offline"]
+                } ${status[member.presence?.status ?? "offline"]}` +
                 "\nCreated:" +
                 ` <t:${Math.floor(user.createdAt.getTime() / 1000)}:D>` +
                 "\nNickname:" +
                 ` ${member.nickname ?? Emojis.Cross}` +
                 "\nStreaming:" +
-                ` ${member.presence?.activities?.filter(
-                  (item) => item.name === "YouTube" || item.name === "Twitch"
-                ).length > 0
-                  ? member.presence?.activities
-                    .filter(
-                      (item) =>
-                        item.name === "YouTube" || item.name === "Twitch"
-                    )
-                    .map((activity) => {
-                      if (activity.type === ActivityType.Streaming) {
-                        return ` ${Emojis.Check}`;
-                      }
-                    })
-                  : ` ${Emojis.Cross}`
+                ` ${
+                  member.presence?.activities?.filter(
+                    (item) => item.name === "YouTube" || item.name === "Twitch"
+                  ).length > 0
+                    ? member.presence?.activities
+                        .filter(
+                          (item) =>
+                            item.name === "YouTube" || item.name === "Twitch"
+                        )
+                        .map((activity) => {
+                          if (activity.type === ActivityType.Streaming) {
+                            return ` ${Emojis.Check}`;
+                          }
+                        })
+                    : ` ${Emojis.Cross}`
                 }` +
                 "\nBot:" +
                 ` ${bot}` +
                 "\nDevices:" +
                 ` ${device()}` +
                 "\nAvatar:" +
-                ` [png](${member.displayAvatarURL({ extension: 'png' })}) • [jpg](${member.displayAvatarURL({ extension: 'jpg' })})`,
+                ` [png](${member.displayAvatarURL({
+                  extension: "png",
+                })}) • [jpg](${member.displayAvatarURL({ extension: "jpg" })})`,
             },
             {
               name: "Roles:",
@@ -246,10 +275,11 @@ export default new SlashClass({
               name: "Presence:",
               value: codeBlock(
                 "ini",
-                `${member.presence?.activities
-                  .filter((item) => item.name != "Custom Status")
-                  .map((activity) => `[${activity.name}]`)
-                  .join("\n") || "No activities"
+                `${
+                  member.presence?.activities
+                    .filter((item) => item.name != "Custom Status")
+                    .map((activity) => `[${activity.name}]`)
+                    .join("\n") || "No activities"
                 }`
               ),
             },
@@ -260,22 +290,24 @@ export default new SlashClass({
             iconURL: member.user.avatarURL({ extension: "png" }),
           })
           .setTimestamp();
-        int.reply({ embeds: [memberEmbed], ephemeral: false });
 
-      } else {
-        // if user isn't a guild member and just a USER
-        const embed = new EmbedBuilder()
-          .setTitle('User Information')
+        return await int.reply({ embeds: [memberEmbed] });
+      } else if (user) {
+        const userEmbed = new EmbedBuilder()
+          .setTitle("User Information")
           .setDescription(`Welcome to ${user.username}'s profile`)
-          .setFields([{
-            name: "General",
-            value: "\nBadges:" +
-              `${badges.join(" ") || " None"}`
-          }]);
-        int.reply({ embeds: [embed], ephemeral: true });
+          .setFields([
+            {
+              name: "General",
+              value: "\nBadges:" + ` ${badges.join(" ") || " None"}`,
+            },
+          ]);
+        return await int.reply({ embeds: [userEmbed] });
       }
     }
+
     if (choice === "guild") {
+
       const embed = new EmbedBuilder().setTitle("Guild information").setFields([
         {
           name: `> ${Emojis.Information} General`,
